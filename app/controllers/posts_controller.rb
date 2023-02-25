ffc683e183f1f6e149818e4abde0c31eea735e6f
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-02-25 09:42:21 -0300

    funcionando tudo certo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
@@ -18,8 +19,19 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post foi atualizado com sucesso'
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
