45dea4a7eda3bab31937430689e71765b0464cc6
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-02-25 08:59:36 -0300

    criando as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/edit'
-  get 'posts/create'
-  get 'posts/update'
-  get 'posts/destroy'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :posts
+  root "pages#home"
 end
