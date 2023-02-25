353dfe9b209606ee0de79e8627401b85be04e0c2
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-02-25 09:30:09 -0300

    criando o edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,14 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def create
