f04f39b29f9636bb1467a3668c92d3973c06d061
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-03-04 09:46:25 -0300

    subindo o controller do feedbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'feedbacks/index'
+  get 'feedbacks/show'
+  get 'feedbacks/new'
+  get 'feedbacks/edit'
+  get 'feedbacks/create'
+  get 'feedbacks/update'
+  get 'feedbacks/destroy'
 
   resources :posts
   root "pages#home"
