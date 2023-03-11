f30ba2e2c401b2bb88d5c86e5565000917de6b25
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-03-11 09:46:07 -0300

    atualizando o repositório local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_11_120557) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_121752) do
+  create_table "feedbacks", force: :cascade do |t|
+    t.text "desc"
+    t.datetime "data"
+    t.boolean "resolvido"
+    t.string "usuario"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
