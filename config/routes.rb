c90ce148a9d1a16611366f64ed7ec98953123e1e
dudafmeneses <mariaeduarda.meneses2608@gmail.com>
2023-03-18 09:49:09 -0300

    arrumando os bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'feedbacks/index'
-  get 'feedbacks/show'
-  get 'feedbacks/new'
-  get 'feedbacks/edit'
-  get 'feedbacks/create'
-  get 'feedbacks/update'
-  get 'feedbacks/destroy'
+  resources :feedbacks
 
   resources :posts
   root "pages#home"
